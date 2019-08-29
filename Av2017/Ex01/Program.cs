fbd2fbd7aa14709e4349415e85611074931c21b2
1164676 <1164676>
2019-08-29 10:14:58 -0300

    Conclusão da questão 01

--- Av2017/Ex01/Program.cs
+++ Av2017/Ex01/Program.cs
@@ -6,11 +6,34 @@ using System.Threading.Tasks;
 
 namespace Ex01
 {
+    class Escola {
+        private string anoLetivo;
+        private decimal anuidade;
+         public Escola(string anoLetivo, decimal anuidade) {
+            this.anoLetivo = anoLetivo;
+            this.anuidade = anuidade;
+        }
+        public void SetAnoLetivo(string anoLetivo) {
+            this.anoLetivo = anoLetivo;
+        }
+        public void SetAnuidade(decimal anuidade) {
+            this.anuidade = anuidade;
+        }
+        public decimal GetValorMensal() {
+            return anuidade / 12;
+        }
+        public decimal GetValorComDesconto() {
+            return anuidade * 90 / 100;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Olá");
+            Escola x = new Escola("1º Ano do Ensino Médio", 12000);
+            Console.WriteLine($"Mensal = {x.GetValorMensal()}");
+            Console.WriteLine($"Anual Desc = {x.GetValorComDesconto()}");
+            Console.ReadKey();
         }
     }
 }
